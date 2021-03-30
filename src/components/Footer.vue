cd8da894f67dabee5f8925ce493fdff14738315b
Linnea <linneaj95@hotmail.com>
2021-03-30 14:57:17 +0200

    Added files for header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+      <ul class="footer__list">
+          <li class="footer__list--item">
+              <h3>Adress:</h3>
+              <p>Södermalm</p>
+              <p>0808 Stockholm</p>
+            </li>
+            <li class="footer__list--item">
+                <h3>Kontakt:</h3>
+                <p>0707-123456</p>
+                <p>info@barkaja.se</p>
+            </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
