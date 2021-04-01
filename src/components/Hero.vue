01066fdd406165a9398877aeafa24c0942e20753
Linnea <linneaj95@hotmail.com>
2021-04-01 13:12:44 +0200

    Added image in hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="hero fullwidth">
     <picture>
-        <img src="https://images.unsplash.com/photo-1518176258769-f227c798150e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1189&q=80" alt="Photo by: Radovan" style="width:auto;">
+        <img src="https://images.unsplash.com/photo-1518176258769-f227c798150e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1189&q=80" alt="Photo by: Radovan">
     </picture>
 </div>
 </template>
