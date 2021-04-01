d965df69ce72d2b2e98b1010a1b66e5f04ab3c5e
Linnea <linneaj95@hotmail.com>
2021-04-01 18:18:58 +0200

    Added text/heading to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,8 @@
 <template>
 <div class="hero fullwidth">
+    <div class="hero__info">
+        <h1>Kaja bar och restaurang</h1>
+    </div>
     <picture>
         <img src="https://images.unsplash.com/photo-1518176258769-f227c798150e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1189&q=80" alt="Photo by: Radovan">
     </picture>
