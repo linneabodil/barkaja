71b828c0609451b29fda85b940f2c7b4c989032e
Linnea <linneaj95@hotmail.com>
2021-04-01 13:14:18 +0200

    Added data and styling for future menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,21 +1,22 @@
 <template>
-<div class="menu fullwidth">
+<div class="menu">
 
     <ul class="menu__food">
         <h2>Matmeny</h2>
-        <li v-for="food in foods" :key="food.name">
-            {{ food.name }}
-            {{ food.ingredients }}
-            {{ food.price }}
+        <li v-for="food in foods" :key="food.name" class="menu__item">
+            <div class="menu__item--name">{{ food.name }}</div>
+            <div class="menu__item--price">{{ food.price }}</div>
+            <p class="menu__item--description">{{ food.ingredients }}</p>
         </li>
     </ul>
 
     <ul class="menu__drinks">
         <h2>Ölmeny</h2>
-        <li v-for="drink in drinks" :key="drink.name">
-            {{ drink.name }}
-            {{ drink.type }}
-            {{ drink.price }}
+        <li v-for="drink in drinks" :key="drink.name" class="menu__item">
+            <div class="menu__item--name">{{ drink.name }}</div>
+            <div class="menu__item--type">{{ drink.type }}</div>
+            <div class="menu__item--price">{{ drink.price }}</div>
+            <p class="menu__item--description">{{ drink.description }}</p>
         </li>
     </ul>
 </div>
