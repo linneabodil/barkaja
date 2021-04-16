24aecad3987ef142a9ac9ee4bd2ef9ab60f24253
Linnea <linneaj95@hotmail.com>
2021-04-16 13:59:02 +0200

    Fixed store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    foods: [
-      {'name': 'Hamburgare',
-      'ingredients': 'Bröd, kött, sallad',
-      'price': 250
-      }
-    ],
-    drinks: [
-      {'name': 'Tail of the Whale',
-      'type': 'Veteöl',
-      'price': 79,
-      'description': 'Öl som smakar väldigt gott'
-      },
-      {'name': 'Brooklyn Defender',
-      'type': 'IPA',
-      'price': 89,
-      'description': 'Öl som smakar gott'
-      }
-    ]
+    foods: [],
+    drinks: []
   },
   getters: {
     getFoods: state => state.foods,
     getDrinks: state => state.drinks
   },
   mutations: {
+    setFoods(state, foods) {
+      state.foods = foods;
+    },
+    setDrinks(state, drinks) {
+      state.drinks = drinks;
+    }
   },
   actions: {
+    async getMenu(data) {
+      let menu = await axios.get('http://gsx2json.com/api?id=ID&sheet=1&columns=false');
+
+      // Sort out food
+      let foods = menu.data.rows.filter(menu => menu.item == "food")
+      data.commit('setFoods', foods);
+      
+      // Sort out drinks
+      let drinks = menu.data.rows.filter(menu => menu.item == "drink")
+      data.commit('setDrinks', drinks);
+    }
   }
 })
