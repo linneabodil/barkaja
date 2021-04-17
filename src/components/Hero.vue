94e97c9a61534c82f30230cd2474081ef88c957b
Linnea <linneaj95@hotmail.com>
2021-04-17 19:52:31 +0200

    Removed image, added text to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,8 @@
 <template>
-<div class="hero fullwidth">
-    <div class="hero__info">
+<div class="hero centered">
+    <div class="hero__heading">
         <h1>Kaja bar och restaurang</h1>
+        <p>Vi är en liten Ivan-ägd krog på Södermalm. Vi erbjuder god dryck och smårätter.</p>
     </div>
-    <picture>
-        <img src="https://images.unsplash.com/photo-1518176258769-f227c798150e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1189&q=80" alt="Photo by: Radovan">
-    </picture>
 </div>
 </template>
