06ac86667723687f7be32a03b5b39060dfcae05e
Linnea <linneaj95@hotmail.com>
2021-05-23 14:51:54 +0200

    Changed text in hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
 <div class="hero centered">
     <div class="hero__heading">
         <h1>Kaja bar och restaurang</h1>
-        <p>Vi är en liten Ivan-ägd krog på Södermalm. Vi erbjuder god dryck och smårätter.</p>
+        <p>En liten krog på söder med hantverksöl, vin och smårätter.</p>
     </div>
 </div>
 </template>
